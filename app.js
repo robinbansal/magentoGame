875efd8039ec9f4d2b48c8322c8dc4884154f048
Robin Bansal <robinbansal55@gmail.com>
2019-11-03 00:54:22 +0530

    Update app.js
    added user input score

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
 var scores,roundScore,activePlayer,gamePlaying;
 
 init();
-
+var lastDice;
 // console.log(dice);
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
-  if(gamePlaying){
-        var dice=Math.floor(Math.random() *6 )+1;
+  if(gamePlaying){ 
+        var dice1=Math.floor(Math.random() *6 )+1;
+        var dice2=Math.floor(Math.random() *6 )+1;
 
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src  = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display  = 'block';
+    document.getElementById('dice-2').style.display  = 'block';
+    document.getElementById('dice-1').src  = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src  = 'dice-' + dice2 + '.png';
 
-    if(dice !== 1){
-        roundScore += dice;
+    if(dice1 !== 1 && dice2 !== 1){
+        roundScore += dice1 + dice2 ;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    }else{
+    }else{  
         nextPlayer(); 
     }
+    /*
+    if(dice==6 && lastDice==6){
+        scores[activePlayer]=0;
+        document.querySelector('#score-'  + activePlayer).textContent = '0';
+        nextPlayer();
     }
+   else if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{  
+        nextPlayer(); 
+    }
+    lastDice =dice;
+ */ 
+    }
+          
 });
-
+    // scores[activePlayer] = setScore;
 
     document.querySelector('.btn-hold').addEventListener('click',function(){
             if(gamePlaying){
                 scores[activePlayer] += roundScore;
+            
                 document.querySelector('#score-'  + activePlayer).textContent = scores[activePlayer];
+                // document.querySelector('#score-'  + activePlayer).textContent = setScore;
+               var input = document.querySelector('.final-score').value;
+               var winningScore;
+                if(input){
+                    winningScore = input;
+                }else{
+                    winningScore = 100;
+                }
     
-                if(scores[activePlayer] >= 100){
+                if(scores[activePlayer] >= winningScore){  
                     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-                    document.querySelector('.dice').style.display = 'none';
+                    document.getElementById('dice-1').style.display = 'none';
+                    document.getElementById('dice-2').style.display = 'none';
                     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
                     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
                     gamePlaying =false;
@@ -38,7 +65,7 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
                     nextPlayer(); 
                 }
             }    
-        });    
+        });   
 
 
         function nextPlayer(){
@@ -51,7 +78,8 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
 
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
         }
 
         document.querySelector('.btn-new').addEventListener('click', init);
@@ -62,7 +90,8 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
             roundScore=0;
             gamePlaying = true;
 
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
 
             document.getElementById('score-0').textContent='0';
             document.getElementById('score-1').textContent='0';
